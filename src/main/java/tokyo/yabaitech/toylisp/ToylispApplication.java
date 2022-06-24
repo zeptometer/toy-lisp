cad9d9bf03e0e38e64203373c655b6fb6c71f160
Yuito Murase <yuito@acupof.coffee>
2022-06-24 21:28:47 +0900

    Initial commit

---
+++ src/main/java/tokyo/yabaitech/toylisp/ToylispApplication.java
@@ -0,0 +1,13 @@
+package tokyo.yabaitech.toylisp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToylispApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToylispApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tokyo/yabaitech/toylisp/ToylispApplicationTests.java
@@ -0,0 +1,13 @@
+package tokyo.yabaitech.toylisp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToylispApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
